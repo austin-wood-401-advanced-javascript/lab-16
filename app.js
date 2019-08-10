f7a0af774a9f8efc68d1fa383b01ffdff039902d
Awood549 <Austin.wood1776@live.com>
2019-08-10 15:50:14 -0700

    small bug fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const files = require('./src/lib/files.js.js');
-require('./src/lib/logger.js.js');
+const files = require('./src/lib/files.js');
+require('./src/lib/logger.js');
 
 let file = process.argv.slice(2).shift();
 files.alterFile(file);
