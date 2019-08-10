12e9e186a3bba98995ca0ede29c6a08c11e4e29d
Awood549 <Austin.wood1776@live.com>
2019-08-10 14:14:05 -0700

    Things Workgit add -A

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 'use strict';
 
-const fs = require('fs');
+const files = require('./lib/files.js');
+require('./lib/logger.js');
 
-const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) {events.emit('err', err)}
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
-    });
-  });
-};
-
-events.on('err', errHandler);
 let file = process.argv.slice(2).shift();
-alterFile(file);
-
-
-
+files.alterFile(file);
